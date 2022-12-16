7aaf7cc2d96f8b593f694abcb62d181d6d75b50e
Sara Barats <sarabarats@Saras-iMac.localdomain>
2022-12-16 10:26:44 -0500

    added footer, final styling and testing

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -11,13 +11,13 @@ function Blog() {
       </div>
       <div className="gpt3__blog-container">
         <div className="gpt3__blog-container_groupA">
-          <Article imgUrl={blog01} date="August 10, 2022"/>
+          <Article imgUrl={blog01} date="August 10, 2022" title="GPT-3 is the Future: Get Ahead Now"/>
         </div>
         <div className="gpt3__blog-container_groupB">
-          <Article imgUrl={blog02} date="September 28, 2022"/>
-          <Article imgUrl={blog03} date="November 1, 2022"/>
-          <Article imgUrl={blog04} date="December 4, 2022"/>
-          <Article imgUrl={blog05} date="December 30, 2022"/>
+          <Article imgUrl={blog02} date="September 28, 2022" title="GPT-3 is the Future: Get Ahead Now"/>
+          <Article imgUrl={blog03} date="November 1, 2022" title="GPT-3 is the Future: Get Ahead Now"/>
+          <Article imgUrl={blog04} date="December 4, 2022" title="GPT-3 is the Future: Get Ahead Now"/>
+          <Article imgUrl={blog05} date="December 30, 2022" title="GPT-3 is the Future: Get Ahead Now"/>
         </div>
       </div>
     </div>
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import gpt3Logo from '../../assets/logo.png';
+import gpt3Logo from '../../assets/logo.svg'
 import './footer.css';
 
 function Footer() {
@@ -9,12 +9,34 @@ function Footer() {
         <h2 className="gradient__text">Step into the future, now</h2>
       </div>
       <div className="gpt3__footer-btn">
-        <p>Request Early Access</p>
+      <button type="button" className="slide">Request Early Access</button>
       </div>
       <div className="gpt3__footer-links">
         <div className="gpt3__footer-links_logo">
           <img src={gpt3Logo} alt="GPT-3 Logo" />
+          <p>Crechterwoord K12 182 DK All Rights Reserved.</p>
         </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Contact</h4>
+          <p>Crechterwoord K12 182 DK AICity, ST 12345</p>
+          <p>123-456-7890</p>
+          <p>discover@gpt3now.com</p>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+      <p>© 2022 GPT-3. All Rights Reserved.</p>
       </div>
     </div>
   )
